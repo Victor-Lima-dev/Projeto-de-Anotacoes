b1c66c820137bf0c18ec6e5cf974ed4aa6cde419
Victor Augusto da Silva Lima <VLIMA06@unipac.com.br>
2024-07-15 21:51:34 -0300

    iniciamos o controlador das tags, agora podemos adicionar uma tag a uma anotacao existente

--- Controllers/Tags.cs
+++ Controllers/Tags.cs
@@ -34,7 +34,43 @@ namespace Back_End.Controllers
         {
 
             //procurar anotacao pelo id
+            //verificar se a tag já existe na anotacao
+            //verificar se a tag já existe no banco
 
+
+            var anotacao = await _context.Anotacoes.Include(a => a.Tags).FirstOrDefaultAsync(a => a.Id == idAnotacao);
+
+            if (anotacao == null)
+            {
+                return NotFound();
+            }
+
+            var tagExistente = await _context.Tags.FirstOrDefaultAsync(t => t.Titulo == tag.Titulo);
+
+            if (tagExistente != null)
+            {
+                // Use a tag existente em vez de criar uma nova
+                anotacao.Tags.Add(tagExistente);
+            }
+            else
+            {
+                // Se a tag não existir, adicione a nova tag à lista
+                anotacao.Tags.Add(tag);
+
+                // Como é uma nova tag, associe a anotação a ela
+
+                tag.Anotacoes.Add(anotacao);
+
+                _context.Tags.Add(tag);
+            }
+
+            //modificar o estado da anotacao
+
+            _context.Entry(anotacao).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return tag;
             
 
         }
