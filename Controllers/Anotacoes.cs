0e3618a54602aad2f12efc93d9c21158ee1e382c
Victor Augusto da Silva Lima <VLIMA06@unipac.com.br>
2024-07-16 19:58:37 -0300

    adicionei a procura por titulo, por tag, um metodo para dicionar a tag na anotacao, para remover, para procurar por tag e um metodo de remover tag

--- Controllers/Anotacoes.cs
+++ Controllers/Anotacoes.cs
@@ -25,11 +25,11 @@ namespace Back_End.Controllers
         public async Task<ActionResult<Anotacao>> CriarAnotacao([FromBody] Anotacao anotacao)
         {
             var tagsFinal = new List<Tag>();
-            
+
             foreach (var tag in anotacao.Tags)
             {
                 var tagExistente = await _context.Tags.FirstOrDefaultAsync(t => t.Titulo == tag.Titulo);
-        
+
                 if (tagExistente != null)
                 {
                     // Use a tag existente em vez de criar uma nova
@@ -43,14 +43,14 @@ namespace Back_End.Controllers
                     tag.Anotacoes.Add(anotacao);
                 }
             }
-        
+
             // Atualize as tags da anotação para as tags finais (existentes ou novas)
             anotacao.Tags = tagsFinal;
-        
+
             _context.Anotacoes.Add(anotacao);
-        
+
             await _context.SaveChangesAsync();
-        
+
             return anotacao;
         }
 
@@ -89,9 +89,9 @@ namespace Back_End.Controllers
                 return BadRequest("Id da anotação diferente do id informado");
             }
 
-              _context.Entry(anotacao).State = EntityState.Modified;
+            _context.Entry(anotacao).State = EntityState.Modified;
 
-              await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
 
             return Ok(anotacao);
         }
@@ -137,6 +137,44 @@ namespace Back_End.Controllers
             await _context.SaveChangesAsync();
 
             return NoContent();
+        }
+
+        [HttpGet("titulo")]
+
+        public async Task<ActionResult<IEnumerable<Anotacao>>> ListarAnotacoesPorTitulo(string titulo)
+        {
+            var anotacoes = await _context.Anotacoes
+        .Where(a => a.Titulo.Contains(titulo))
+        .Select(a => new Anotacao
+        {
+            Id = a.Id,
+            Titulo = a.Titulo,
+            Conteudo = a.Conteudo,
+            Tags = a.Tags.Select(t => new Tag { Id = t.Id, Titulo = t.Titulo }).ToList() // Ajuste para incluir Tags
+        })
+        .ToListAsync();
+
+
+            return anotacoes;
+        }
+
+        [HttpGet("idTag")]
+
+        public async Task<ActionResult<IEnumerable<Anotacao>>> ListarAnotacoesPorTag(Guid idTag)
+        {
+            var anotacoes = await _context.Anotacoes
+        .Where(a => a.Tags.Any(t => t.Id == idTag)) // Ajuste para filtrar por ID da tag
+        .Select(a => new Anotacao
+        {
+            Id = a.Id,
+            Titulo = a.Titulo,
+            Conteudo = a.Conteudo,
+            Tags = a.Tags.Select(t => new Tag { Id = t.Id, Titulo = t.Titulo }).ToList() // Inclui Tags
+        })
+        .ToListAsync();
+
+           
+            return anotacoes;
         }
 
     }
--- Controllers/Tags.cs
+++ Controllers/Tags.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore;
 namespace Back_End.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     public class Tags : ControllerBase
     {
         private readonly AppDbContext _context;
@@ -32,12 +32,6 @@ namespace Back_End.Controllers
 
         public async Task<ActionResult<Tag>> AnotacaoAddTag(Guid idAnotacao,[FromBody] Tag tag)
         {
-
-            //procurar anotacao pelo id
-            //verificar se a tag já existe na anotacao
-            //verificar se a tag já existe no banco
-
-
             var anotacao = await _context.Anotacoes.Include(a => a.Tags).FirstOrDefaultAsync(a => a.Id == idAnotacao);
 
             if (anotacao == null)
@@ -71,9 +65,73 @@ namespace Back_End.Controllers
             await _context.SaveChangesAsync();
 
             return tag;
-            
-
         }
 
+        [HttpDelete("{idAnotacao}/{idTag}")]
+        public async Task<ActionResult<Tag>> AnotacaoRemoveTag(Guid idAnotacao, Guid idTag)
+        {
+            var anotacao = await _context.Anotacoes.Include(a => a.Tags).FirstOrDefaultAsync(a => a.Id == idAnotacao);
+
+            if (anotacao == null)
+            {
+                return NotFound("Anotação não encontrada");
+            }
+
+            // Verificar se a tag existe no bd
+
+            var tagBd = await _context.Tags.FirstOrDefaultAsync(t => t.Id == idTag);
+
+            if (tagBd == null)
+            {
+                return NotFound("Tag não encontrada");
+            }
+
+            var tag = anotacao.Tags.FirstOrDefault(t => t.Id == idTag);
+
+            if (tag == null)
+            {
+                return NotFound("A anotação não possui essa tag");
+            }
+
+            anotacao.Tags.Remove(tag);
+
+            _context.Entry(anotacao).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return tag;
     }
-}
+
+    [HttpDelete("{idTag}")]
+    public async Task<ActionResult<Tag>> DeleteTag(Guid idTag)
+    {
+        var tag = await _context.Tags.Include(t => t.Anotacoes).FirstOrDefaultAsync(t => t.Id == idTag);
+
+        if (tag == null)
+        {
+            return NotFound();
+        }
+
+
+        _context.Tags.Remove(tag);
+
+        await _context.SaveChangesAsync();
+
+        return tag;
+    }
+
+    [HttpGet("{titulo}")]
+
+    public async Task<ActionResult<Tag>> GetTag(string titulo)
+    {
+        var tag = await _context.Tags.Include(t => t.Anotacoes).FirstOrDefaultAsync(t => t.Titulo == titulo);
+
+        if (tag == null)
+        {
+            return NotFound();
+        }
+
+        return tag;
+
+    }
+}}
